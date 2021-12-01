e54b55c519d987c9098372f882744e65b5cfd027
locserd <3705270+locserd@users.noreply.github.com>
2021-12-01 19:31:53 -0300

    Desafio Contador JS

---
+++ JS/contador.js
@@ -0,0 +1,10 @@
+let cont = 0;
+var contador = document.getElementById('numeroAtual');
+function add() {
+  cont++
+  contador.innerHTML = cont;
+}
+function sub() {
+  cont--
+  contador.innerHTML = cont;
+}
